35381f1ff5e31eefcde31b0d4f57f9ac16959318
Altod69 <kelvinfreeze@hotmail.com>
2018-07-22 15:23:17 +0200

    last Maj

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,6 @@
 function rotateArrow()
 {
     alert("son of bitch!");
-    //var cubeArrow = document.getElementsById('redArrow');
-    //cubeArrow.style.transform = 'rotate(90deg)';
-    //document.getElementsById('redArrow').style.transform = 'rotate(90deg)';
+    var cubeArrow = document.getElementsById("redArrow");
+    cubeArrow.style.transform = "rotate(90deg)";
 }
